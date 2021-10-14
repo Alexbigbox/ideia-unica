098862ed9840cb6e4a53684f34b7abfe61384042
Alexander kruger <alex.bigbox@gmail.com>
2021-10-14 00:12:30 -0300

    inscritos

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,16 @@
-function tempo(request, response) {
+async function tempo(request, response) {
   const dynamicDate = new Date();
 
+  const subscribersResponse = await 
+    fetch("https://api.convertkit.com/v3/subscribers?api_secret=ZJwLOhi01EvB90CIpsyw1e0fGRzPfw1Q");
+
+  const subscribersResponseJson = await subscribersResponse.json();
+  const inscritos = subscribersResponseJson.total_subscribers;
+
   response.json({
-    date: dynamicDate.toGMTString()
-  })
+    date: dynamicDate.toGMTString(),
+    inscritos: inscritos
+  });
 }
 
 export default tempo;
