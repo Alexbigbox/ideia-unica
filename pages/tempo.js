b96b5818a61e6c760fc7301a9f58b02d0959ea81
Alexander kruger <alex.bigbox@gmail.com>
2021-10-13 23:39:14 -0300

    Revalidate

--- pages/tempo.js
+++ pages/tempo.js
@@ -6,26 +6,24 @@ function Tempo(props) {
   return (
     <div>
       <div> {dinamicDateString} (dinâmico)</div>
-      <div> {props.staticDateString} (estático - com delay) </div>
+      <div> {props.staticDateString} (estático) </div>
     </div>
   )
 }
 
 export async function getStaticProps() {
   console.log('> Passando pela getStaticProps;')
-  console.log('> Adicionado delay de 5 segundos;')
 
-  await delay(5000);
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
 
   return {
     props: {
       staticDateString
-    }
+    },
+    revalidate: 1
   }
 }
 
-const delay = ms => Promise(resolve => setTimeout(resolve, ms))
 
 export default Tempo;
